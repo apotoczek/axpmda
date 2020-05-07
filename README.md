101a838e22770803372587765516634ddb33fb4a
apotoczek <adam@tondoinc.com>
2020-05-07 02:54:32 -0400

    axpmda-7 streaks calculation

--- README.md
+++ README.md
@@ -70,7 +70,7 @@ Run server and verify superuser
 
 `(axpmda) adx5000@~/git/axpmda (develop)$ ./manage.py runserver`
 
-Go to http://127.0.0.1:8000/ to load default Django home page, then go to http://127.0.0.1:8000/admin/ and login as the superuser
+Go to 127.0.0.1:8000/ to load default Django home page (before adding any urlpatterns, afterwards no home page exists, only API endpoints), then go to 127.0.0.1:8000/admin/ and login as the superuser
 
 ## API Functionality
 
@@ -80,17 +80,55 @@ Users can login/logout via Login link on /mood/v1/ or directly at /api-auth/logi
 
 **Endpoints**
 
-- get report: 127.0.0.1:8000/mood/v1/
-- get list: 127.0.0.1:8000/mood/v1/moods/
-- get detail: 127.0.0.1:8000/mood/v1/moods/1/ (specify <int:pk> like 1 if it exists)
+* GET Mood Report: 127.0.0.1:8000/mood/v1/
+    * shows your user's info, streaks and list of moods
+
+```
+"user_id": your user's id
+"username": your user's username
+"moods_count": count of moods for your user
+"newest_streak": newest streak of mood entries on consecutive days
+"longest_streak": longest overall streak in your mood entries history
+"streak_percentile": displayed in decimal representation...
+    your longest streak = your_longest
+    avg of longest streaks from all other users = avg_longest
+	(your_longest - avg_longest) / avg_longest 
+	e.g.: (7 - 3.8) / 3.8 = 0.84, which makes your percentile 84%
+	negative decimal indicates below 0%
+    return value can be easily changed to True/False for >=50%
+        but the decimal representation may be more useful
+```
+
+* GET Mood List: 127.0.0.1:8000/mood/v1/moods/
+    * shows your user's list of moods
+  
+* POST Mood: 127.0.0.1:8000/mood/v1/moods/
+    * submit new mood for your user, e.g.:
+ 
+```
+Media type: application/json
+
+Content:
+{
+    "mood": "<CharField max_length=50 required>",
+    "details": "<TextField optional>"
+}
+```
+
+* GET Mood Detail: 127.0.0.1:8000/mood/v1/moods/(mood id int:pk)/
+    * shows single mood using mood id, if it exists for your user
 
 Note: you must be logged-in and can only view, add or modify moods associated with your user
 
 ## Production
 
-Notes for production deployment:
+Possible TODOs for production deployment:
 
 - segregate django settings into enviornments; **base** settings for all envs, **local** settings, **prod** settings (and any other envs desired)
+- add env-specific logging / debug settings
 - include a credentials file pulled from something like a private S3 bucket with env vars/creds for each enviornment
+- setup CI/CD like Jenkins to trigger based on changes to **master** branch
 - add CORS and whitelist correct domains
 - for AWS, setup EB deployment
+- refactor streak calculations into models or DRYer solutions
+- add API endpoints for user registration/auth etc. (currently a superuser can add users)
--- moods/serializers.py
+++ moods/serializers.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import get_user_model, get_user
+from datetime import datetime
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Mood
 
@@ -6,24 +7,158 @@ from .models import Mood
 class MoodSerializer(serializers.ModelSerializer):
 
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    mood_id = serializers.IntegerField(source='id')
 
     class Meta:
         model = Mood
-        fields = ('mood_id', 'user', 'mood', 'details', 'created_at',)
+        fields = ('id', 'user', 'mood', 'details', 'created_at',)
 
 
 class ReportSerializer(serializers.ModelSerializer):
 
+    user_id = serializers.IntegerField(source='id')
     moods = MoodSerializer(many=True, read_only=True)
+
     moods_count = serializers.SerializerMethodField(method_name='_moods_count')
 
-    def _moods_count(self, instance):
+    def _moods_count(self, obj):
         moods = Mood.objects.filter(user=self.context['request'].user)
         return len(moods)
 
-    user_id = serializers.IntegerField(source='id')
+    newest_streak = serializers.SerializerMethodField(method_name='_newest_streak')
+
+    def _newest_streak(self, obj):
+        newest_streak = 1
+
+        today = datetime.today()
+        entry_dates = list(Mood.objects.values("created_at")
+                           .filter(user=self.context['request'].user, created_at__lte=today)
+                           .order_by("-created_at"))
+        length = len(entry_dates)
+
+        if length == 0:
+            return 0
+        elif length == 1:
+            return 1
+        else:
+            i = 0
+            newest_streak_broken = False
+            while i < length - 1:
+                delta = entry_dates[i]['created_at'].date() - entry_dates[i+1]['created_at'].date()
+                if delta.days == 1 and not newest_streak_broken:
+                    newest_streak += 1
+                elif delta.days == 0 and not newest_streak_broken:
+                    newest_streak += 0  # pass
+                else:
+                    newest_streak_broken = True
+                i += 1
+
+        return newest_streak
+
+    longest_streak = serializers.SerializerMethodField(method_name='_longest_streak')
+
+    def _longest_streak(self, obj):
+        current_streak = 1
+        longest_streak = 1
+
+        today = datetime.today()
+        entry_dates = list(Mood.objects.values("created_at")
+                           .filter(user=self.context['request'].user, created_at__lte=today)
+                           .order_by("-created_at"))
+        length = len(entry_dates)
+
+        if length == 0:
+            return 0
+        elif length == 1:
+            return 1
+        else:
+            i = 0
+            while i < length - 1:
+                delta = entry_dates[i]['created_at'].date() - entry_dates[i + 1]['created_at'].date()
+                if delta.days == 1:
+                    current_streak += 1
+                    if current_streak > longest_streak:
+                        longest_streak = current_streak
+                elif delta.days == 0:
+                    current_streak += 0  # pass
+                else:
+                    current_streak = 1
+                i += 1
+
+        return longest_streak
+
+    streak_percentile = serializers.SerializerMethodField(method_name='_streak_percentile')
+
+    def _streak_percentile(self, obj):
+        today = datetime.today()
+        my_current_streak = 1
+        my_longest_streak = 1
+        entry_dates = list(Mood.objects.values("created_at")
+                           .filter(user=self.context['request'].user, created_at__lte=today)
+                           .order_by("-created_at"))
+        length = len(entry_dates)
+        if length == 0:
+            my_longest_streak = 0
+        elif length == 1:
+            my_longest_streak = 1
+        else:
+            i = 0
+            while i < length - 1:
+                delta = entry_dates[i]['created_at'].date() - entry_dates[i + 1]['created_at'].date()
+                if delta.days == 1:
+                    my_current_streak += 1
+                    if my_current_streak > my_longest_streak:
+                        my_longest_streak = my_current_streak
+                elif delta.days == 0:
+                    my_current_streak += 0  # pass
+                else:
+                    my_current_streak = 1
+                i += 1
+
+        users = get_user_model().objects.all()
+        others_longest_streaks = []
+        for u in users:
+            others_current_streak = 1
+            others_longest_streak = 1
+            if u.id != self.context['request'].user.id:
+                entry_dates = list(Mood.objects.values("created_at")
+                                   .filter(user=u, created_at__lte=today)
+                                   .order_by("-created_at"))
+                length = len(entry_dates)
+                if length == 0:
+                    others_longest_streak = 0
+                elif length == 1:
+                    others_longest_streak = 1
+                else:
+                    i = 0
+                    while i < length - 1:
+                        delta = entry_dates[i]['created_at'].date() - entry_dates[i + 1]['created_at'].date()
+                        if delta.days == 1:
+                            others_current_streak += 1
+                            if others_current_streak > others_longest_streak:
+                                others_longest_streak = others_current_streak
+                        elif delta.days == 0:
+                            others_current_streak += 0  # pass
+                        else:
+                            others_current_streak = 1
+                        i += 1
+                others_longest_streaks.append(others_longest_streak)
+
+        avg_others_longest_streak = 0
+        for x in others_longest_streaks:
+            avg_others_longest_streak += x
+        avg_others_longest_streak = avg_others_longest_streak / len(others_longest_streaks)
+        streak_percentile_decimal = ((my_longest_streak - avg_others_longest_streak) / avg_others_longest_streak)
+
+        return round(streak_percentile_decimal, 2)
 
     class Meta:
         model = get_user_model()
-        fields = ('user_id', 'username', 'moods_count', "moods",)
+        fields = (
+            'user_id',
+            'username',
+            'moods_count',
+            'newest_streak',
+            'longest_streak',
+            'streak_percentile',
+            'moods',
+        )
